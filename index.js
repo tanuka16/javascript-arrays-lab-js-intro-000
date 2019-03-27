e7aca0fec462cdccff8e73dce8b6dfa1447536da
Tanuka Das <das.tanuka@yahoo.com>
2019-03-27 17:49:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function removeLastKitten(){
   //kittens.slice(kittens.length - 1);
   return kittens.slice(0, kittens.length - 1);
 }
-funtion removeFirstKitten(){
-  return kittens.slice();
+function removeFirstKitten(){
+  return kittens.slice(1);
 }
