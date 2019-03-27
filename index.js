646b03f783526f230c9789ae8c1aded97d4bafaa
Tanuka Das <das.tanuka@yahoo.com>
2019-03-27 17:24:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function prependKitten(name){
   //var kittens = ["Milo", "Otis", "Garfield"]
   kittens;              // ["Philadelphia", ...cities]
   var array1 = new Array();
-  array1 = [...array1, "Arnold"]
+  
+  //array1 = [...array1, "Arnold"]
   return kittens.concat(array1);
 }
